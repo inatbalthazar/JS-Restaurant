ac7cdccf4b6d5bb1fd218a5a2ba2cfd4dc120e0e
inatbalthazar <inatbalthazar@gmail.com>
2026-07-02 14:00:07 +0800

    kitchen

---
+++ my-array.js
@@ -0,0 +1,9 @@
+let food = ["pizza", "burger", "pasta", "sushi", "tacos", "pad thai", "ramen", "steak"];
+console.log(food[0]); // Output: pizza
+console.log(food[1]); // Output: burger
+console.log(food[2]); // Output: pasta
+console.log(food[3]); // Output: sushi
+console.log(food[4]); // Output: tacos
+console.log(food[5]); // Output: pad thai
+console.log(food[6]); // Output: ramen
+console.log(food[7]); // Output: steak
---
+++ my-object.js
@@ -0,0 +1,69 @@
+// let foodDatabase = [
+//     { id: 1, name: "pizza", price: 12.99, size: ["small", "medium", "large"] },
+//     { id: 2, name: "burger", price: 10.99, size: ["small", "medium", "large"] },
+//     { id: 3, name: "pasta", price: 11.99, size: ["small", "medium", "large"] },
+//     { id: 4, name: "sushi", price: 14.99, size: ["small", "medium", "large"] },
+//     { id: 5, name: "tacos", price: 9.99, size: ["small", "medium", "large"] },
+//     { id: 6, name: "pad thai", price: 13.99, size: ["small", "medium", "large"] },
+//     { id: 7, name: "ramen", price: 12.99, size: ["small", "medium", "large"] },
+//     { id: 8, name: "steak", price: 19.99, size: ["small", "medium", "large"] }
+// ]
+
+let foodDatabase = [
+    {
+        id: 1,
+        name: "pizza",
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 2,
+        name: "burger",
+        cuisine: "American",
+        origin: "USA",
+        vegetarian: false,
+    },
+    {
+        id: 3,
+        name: "pasta",
+        cuisine: "Italian",
+        origin: "Italy",
+        vegetarian: true,
+    },
+    {
+        id: 4,
+        name: "sushi",
+        cuisine: "Japanese",
+        origin: "Japan",
+        vegetarian: false,
+    },
+    {
+        id: 5,
+        name: "tacos",
+        cuisine: "Mexican",
+        origin: "Mexico",
+        vegetarian: false,
+    },
+    {
+        id: 6,
+        name: "pad thai",
+        cuisine: "Thai",
+        origin: "Thailand",
+        vegetarian: true,
+    },
+    {
+        id: 7,
+        name: "ramen",
+        cuisine: "Japanese",
+        origin: "Japan",
+        vegetarian: false,
+    },
+    {
+        id: 8,
+        name: "steak",
+        cuisine: "American",
+        origin: "USA",
+        vegetarian: false,
+    }
+]
